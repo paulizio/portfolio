4e5cc9f4541117bacabe6d967db779b3cfbd7b5b
paulizio <pauli.nordman@hotmail.com>
2021-02-21 20:49:01 +0200

    changed contact color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,14 @@ const Contact = () => {
   return (
     <Container fluid className='d-flex vh-50 justify-content-center align-items-center' id='contact'>
       <Row className='align-items-center justify-content-center text-center my-5'>
-        <h1>Get in touch with me</h1>
+        <h1 style={{ color: 'white' }}>Get in touch with me</h1>
       </Row>
       <Row className='align-items-center justify-content-center text-center my-5'>
         <Col className='align-items-center justify-content-center text-center my-5'>
-          <a href='https://www.linkedin.com/in/pauli-nordman/' target='_blank' rel='noreferrer'><AiFillLinkedin size={80} color='black' id='icon-hov' /></a>
+          <a href='https://www.linkedin.com/in/pauli-nordman/' target='_blank' rel='noreferrer'><AiFillLinkedin size={80} color='white' id='icon-hov' /></a>
         </Col>
         <Col className='align-items-center justify-content-center text-center my-5'>
-          <a href='https://www.github.com/paulizio' target='_blank' rel='noreferrer'><AiFillGithub size={80} color='black' id='icon-hov' /></a>
+          <a href='https://www.github.com/paulizio' target='_blank' rel='noreferrer'><AiFillGithub size={80} color='white' id='icon-hov' /></a>
         </Col>
       </Row>
     </Container>
